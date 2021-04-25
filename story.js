3ebb01a2aa628aaefa7327afef0bfe18b1bee56b
joewatt40 <wat19030@byui.edu>
2021-04-24 22:21:30 -0700

    added story editor files and link to assignment

--- story.js
+++ story.js
@@ -1,10 +1,14 @@
-function newWeek(){
-    
+function loadStory(){
+    var storyName = document.getElementById("name_input").value
+    var storyHTML = localStorage.getItem(storyName)
+    document.getElementById("story_editor").value = storyHTML
 }
-
-const links = [
-    {
-      label: "Week1 notes",
-      url: "week1/index.html"
-    }
-  ]
+function saveStory(){
+    var storyName = document.getElementById("name_input").value
+    var storyHTML = document.getElementById("story_editor").value
+    localStorage.setItem(storyName, storyHTML)
+}
+function displayStory(){
+    var storyHTML = document.getElementById("story_editor").value
+    document.getElementById("story_display").innerHTML = storyHTML
+}

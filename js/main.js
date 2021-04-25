cec97b3faadb9c1c34376feb40c1ba2baff9756f
joewatt40 <wat19030@byui.edu>
2021-04-24 17:15:27 -0700

    update js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,5 @@ for(i = 0; i < links.length; i++) {
     var newItem = document.createElement('li')
     var x = document.createElement('a')
     list.appendChild(newItem)
-    list.appendChild(x)
     newItem.innerHTML = links[i]["label"]
-    x.innerHTML = links[i]["url"]
 }

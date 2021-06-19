3226d77d5584a03b380a5889a0be8674dfd2bbb3
joewatt40 <wat19030@byui.edu>
2021-06-19 16:56:55 -0700

    updat group files

---
+++ week9/group/group.js
@@ -0,0 +1,8 @@
+window.addEventListener('keydown', function(e) {
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!sound)return;
+    sound.currentTime = 0;
+    sound.play();
+    console.dir(sound);
+    // console.log(sound);
+});
